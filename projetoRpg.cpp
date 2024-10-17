bedd815efed866e54b50fdfdf399b3216c0cabf3
Sávio Decaro <142761864+SavioDec@users.noreply.github.com>
2024-10-17 19:28:28 -0300

    correção do mapa para liberar os caminhos

--- projetoRpg.cpp
+++ projetoRpg.cpp
@@ -21,7 +21,7 @@ public:
     void sobeNivel()
     {
         int escolha;
-        cout << "Subiu de nível!!" << endl
+        cout << "Subiu de nivel!!" << endl
              << "Qual atributo deseja upar?" << endl;
         cout << "1- Ataque - " << ataque << endl;
         cout << "2- Defesa - " << defesa << endl;
@@ -48,97 +48,7 @@ public:
     }
 
 
-    class Mapa {
-public:
-    int posX = 0;
-    int posY = 0;
-    const int largura = 5;
-    const int altura = 5;
-
-    int mapa[5][5] = {
-        {0, 0, 1, 0, 0},
-        {0, 0, 1, 0, 0},
-        {0, 0, 1, 0, 0},
-        {0, 0, 1, 1, 1},
-        {0, 0, 0, 1, 1}
-    };
-    void mostrarOpcoes() {
-        cout << "Você está na sala (" << posX << ", " << posY << ")." << endl;
-        cout << "Movimentos disponíveis:" << endl;
-
-        if (posX > 0) cout << "1- Frente" << endl;
-        if (posX < altura - 1) cout << "2- Trás" << endl;
-
-        // Verificar se esquerda/direita estão bloqueadas
-        if (posY > 0 && mapa[posX][posY - 1] == 0) {
-            cout << "3- Esquerda" << endl;
-        } else {
-            cout << "Esquerda bloqueada!" << endl;
-        }
-
-        if (posY < largura - 1 && mapa[posX][posY + 1] == 0) {
-            cout << "4- Direita" << endl;
-        } else {
-            cout << "Direita bloqueada!" << endl;
-        }
-    }
-
-    // Movimenta o jogador de acordo com a opção
-    void mover(int opcao) {
-        switch (opcao) {
-        case 1:
-            if (posX > 0) {
-                posX--;
-                cout << "Você se moveu para frente." << endl;
-            } else {
-                cout << "Movimento inválido!" << endl;
-            }
-            break;
-        case 2:
-            if (posX < altura - 1) {
-                posX++;
-                cout << "Você se moveu para trás." << endl;
-            } else {
-                cout << "Movimento inválido!" << endl;
-            }
-            break;
-        case 3:
-            if (posY > 0 && mapa[posX][posY - 1] == 0) {
-                posY--;
-                cout << "Você se moveu para a esquerda." << endl;
-            } else {
-                cout << "Movimento inválido!" << endl;
-            }
-            break;
-        case 4:
-            if (posY < largura - 1 && mapa[posX][posY + 1] == 0) {
-                posY++;
-                cout << "Você se moveu para a direita." << endl;
-            } else {
-                cout << "Movimento inválido!" << endl;
-            }
-            break;
-        default:
-            cout << "Opção inválida!" << endl;
-            break;
-        }
-    }
-
-    // Função para iniciar o mapa e movimentação
-    void iniciarMovimentacao() {
-        int opcao = 0;
-        while (true) {
-            mostrarOpcoes();
-            cout << "Escolha um movimento (0 para sair): ";
-            cin >> opcao;
-            if (opcao == 0) {
-                cout << "Saindo do mapa." << endl;
-                break;
-            }
-            mover(opcao);
-        }
-    }
-};
+    
 
     // void magiaAtaque() {
     //     if (vida < 100) {
@@ -147,7 +57,7 @@ public:
     //             vida = 100;
     //         }
     //     } else {
-    //         cout << "Sua vida está cheia!" << endl;
+    //         cout << "Sua vida esta cheia!" << endl;
     //     }
     // }
 
@@ -162,7 +72,7 @@ public:
                 return;
             }
         }
-        cout << "Mochila cheia! Não é possível adicionar mais itens." << endl;
+        cout << "Mochila cheia! Nao é possivel adicionar mais itens." << endl;
     }
 
     void usarItem()
@@ -201,7 +111,7 @@ public:
         }
         else
         {
-            cout << "Item não encontrado." << endl;
+            cout << "Item nao encontrado." << endl;
         }
     }
 };
@@ -220,10 +130,101 @@ public:
         
         int danoInimigo = (rand() % 20) - player.defesa; // Dano aleatório do inimigo
         if (danoInimigo < 5)
-            danoInimigo = 5; // O dano não pode ser negativo
+            danoInimigo = 5; // O dano nao pode ser negativo
         player.vida -= danoInimigo;
         cout << "Inimigo atacou, causando " << danoInimigo << " de dano!" << endl;
         return danoInimigo;
+    }
+};
+class Mapa {
+public:
+    int posX = 0;
+    int posY = 0;
+    const int largura = 5;
+    const int altura = 5;
+
+    int mapa[5][5] = {
+        {0, 0, 1, 0, 0},
+        {0, 0, 0, 1, 0},
+        {1, 0, 1, 0, 0},
+        {0, 0, 0, 0, 1},
+        {1, 0, 0, 1, 1}
+    };
+    void mostrarOpcoes() {
+        cout << "Voce esta na sala (" << posX << ", " << posY << ")." << endl;
+        cout << "Movimentos disponiveis:" << endl;
+
+        if (posX > 0) cout << "1- Frente" << endl;
+        if (posX < altura - 1) cout << "2- Tras" << endl;
+
+        // Verificar se esquerda/direita estao bloqueadas
+        if (posY > 0 && mapa[posX][posY - 1] == 0) {
+            cout << "3- Esquerda" << endl;
+        } else {
+            cout << "Esquerda bloqueada!" << endl;
+        }
+
+        if (posY < largura - 1 && mapa[posX][posY + 1] == 0) {
+            cout << "4- Direita" << endl;
+        } else {
+            cout << "Direita bloqueada!" << endl;
+        }
+    }
+
+    // Movimenta o jogador de acordo com a opcao
+    void mover(int opcao) {
+        switch (opcao) {
+        case 1:
+            if (posX > 0) {
+                posX--;
+                cout << "Voce se moveu para frente." << endl;
+            } else {
+                cout << "Movimento invalido!" << endl;
+            }
+            break;
+        case 2:
+            if (posX < altura - 1) {
+                posX++;
+                cout << "Voce se moveu para tras." << endl;
+            } else {
+                cout << "Movimento invalido!" << endl;
+            }
+            break;
+        case 3:
+            if (posY > 0 && mapa[posX][posY - 1] == 0) {
+                posY--;
+                cout << "Voce se moveu para a esquerda." << endl;
+            } else {
+                cout << "Movimento invalido!" << endl;
+            }
+            break;
+        case 4:
+            if (posY < largura - 1 && mapa[posX][posY + 1] == 0) {
+                posY++;
+                cout << "Voce se moveu para a direita." << endl;
+            } else {
+                cout << "Movimento invalido!" << endl;
+            }
+            break;
+        default:
+            cout << "Opcao invalida!" << endl;
+            break;
+        }
+    }
+
+    // Funcao para iniciar o mapa e movimentacao
+    void iniciarMovimentacao() {
+        int opcao = 0;
+        while (true) {
+            mostrarOpcoes();
+            cout << "Escolha um movimento (0 para sair): ";
+            cin >> opcao;
+            if (opcao == 0) {
+                cout << "Saindo do mapa." << endl;
+                break;
+            }
+            mover(opcao);
+        }
     }
 };
 
@@ -235,11 +236,11 @@ void inimigoAleatorio(Player &player, Inimigo &inimigo)
     inimigo.vida = (rand() % 50) + 50; // Reiniciar a vida do inimigo
     if (player.ataque < inimigo.defesa)
     {
-        inimigo.defesa = (rand() % 20) + 5; // Definição de defesa aleatória
+        inimigo.defesa = (rand() % 20) + 5; // Definicao de defesa aleatória
     }
     else
     {
-        inimigo.defesa = (rand() % 5) + 5; // Definição de defesa aleatória
+        inimigo.defesa = (rand() % 5) + 5; // Definicao de defesa aleatória
     }
 }
 
@@ -249,7 +250,7 @@ void atacarInimigo(Inimigo &inimigo, Player &player)
     srand(seed);
     int dado = rand() % 20;
     int dano = player.ataque - inimigo.defesa;
-    if (dano < 0) dano = 0; // O dano não pode ser negativo
+    if (dano < 0) dano = 0; // O dano nao pode ser negativo
     if(dado > 18){
         cout << "dano critico!! causado: "<< dano << endl;
         dano = (player.ataque + 10) - inimigo.defesa;
@@ -292,7 +293,7 @@ void conjuraMagia(Player &player, Inimigo &inimigo)
             if (dado > 18)
             {
                 int dano = player.magia + 15;
-                if (dano < 0) dano = 0; // O dano não pode ser negativo
+                if (dano < 0) dano = 0; // O dano nao pode ser negativo
                 cout << "Dano critico!!!!\nBola de fogo foi conjurado! Causando: " << dano << " de dano" << endl;
                 player.mana -= 10;
                 inimigo.vida -= dano;
@@ -305,13 +306,13 @@ void conjuraMagia(Player &player, Inimigo &inimigo)
             else if (dado > 2)
             {
                 int dano = player.magia;
-                if (dano < 0) dano = 0; // O dano não pode ser negativo
+                if (dano < 0) dano = 0; // O dano nao pode ser negativo
                 cout << "Bola de fogo foi conjurada! \nCausando: " << dano << " de dano" << endl;
                 player.mana -= 10;
                 inimigo.vida -= dano;
             }
         }else{
-            cout << "Você não tem mana suficiente para conjurar a bola de fogo!" << endl;
+            cout << "Voce nao tem mana suficiente para conjurar a bola de fogo!" << endl;
         }
     }
     if (opcao == 2)
@@ -337,7 +338,7 @@ void conjuraMagia(Player &player, Inimigo &inimigo)
                 player.mana -= 5;
             }
         }else{
-            cout << "Você não tem mana suficiente para curar!" << endl;
+            cout << "Voce nao tem mana suficiente para curar!" << endl;
         }
     }
     if (opcao == 3)
@@ -348,7 +349,7 @@ void conjuraMagia(Player &player, Inimigo &inimigo)
             if (dado > 18)
             {
                 int dano = player.magia + 25;
-                if (dano < 0) dano = 0; // O dano não pode ser negativo
+                if (dano < 0) dano = 0; // O dano nao pode ser negativo
                 cout << "Dano critico!!!!\nLanca de raio foi conjurada! Causando: " << dano << " de dano" << endl;
                 player.mana -= 35;
                 inimigo.vida -= dano;
@@ -361,13 +362,13 @@ void conjuraMagia(Player &player, Inimigo &inimigo)
             else if (dado > 2)
             {
                 int dano = player.magia + 10;
-                if (dano < 0) dano = 0; // O dano não pode ser negativo
+                if (dano < 0) dano = 0; // O dano nao pode ser negativo
                 cout << "Lanca de raio foi conjurada! \nCausando: " << dano << " de dano" << endl;
                 player.mana -= 35;
                 inimigo.vida -= dano;
             }
         }else{
-            cout << "Você não tem mana suficiente para conjurar a lanca de raio!" << endl;
+            cout << "Voce nao tem mana suficiente para conjurar a lanca de raio!" << endl;
         }
     }
 }
@@ -452,6 +453,6 @@ int main()
     if (player.vida < 1)
     {
 
-        cout << "Você perdeu!!" << endl;
+        cout << "Voce perdeu!!" << endl;
     }
 }
